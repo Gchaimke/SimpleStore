cbc2b8e8b3772c7a1578eb9f9aa6cf0aabb92ffb
Chaim Gorbov <Gchaimke@gmail.com>
2021-04-28 17:41:21 +0300

    Add files via upload

---
+++ elements/card.php
@@ -0,0 +1,10 @@
+<div class="card mt-2" style="width: 18rem;">
+    <div class="card-image" style="background-image: url('<?= $product['img'] ?>');"></div>
+    <div class="card-body">
+        <h5 class="card-title"><?= $product['name'] ?></h5>
+        <p class="card-text"><?= $product['description'] ?></p>
+        <div class="d-flex justify-content-center">
+            <h5 class="card-text me-3"><?= $product['price'] ?> ש"ח</h5>
+        </div>
+    </div>
+</div>
