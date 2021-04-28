c4be8a660d3ba71c8eeb408ec4361069afb7eeb1
Chaim Gorbov <Gchaimke@gmail.com>
2021-04-28 17:42:09 +0300

    Add files via upload

---
+++ body.php
@@ -0,0 +1,199 @@
+<?php
+$categories = array(
+    "Vse Myaso" => array(
+        array(
+            'id' => 1,
+            'name' => 'myaso',
+            'description' => 'good',
+            'price' => '65',
+            'img' => 'https://cdn.profile.ru/wp-content/uploads/2019/08/shutterstock_1050939104-782x440.jpg',
+        ),
+        array(
+            'id' => 2,
+            'name' => 'riba',
+            'description' => 'good',
+            'price' => '40',
+            'img' => 'http://capriceeurope.eu/assets/uploads/2020/04/Pescadoo-e1587726895841.jpg',
+        ),
+        array(
+            'id' => 3,
+            'name' => 'yaica',
+            'description' => 'good',
+            'price' => '15',
+            'img' => 'https://liter.kz/wp-content/uploads/2020/10/pkyblkr-750x422.jpg',
+        ),
+        array(
+            'id' => 2,
+            'name' => 'riba',
+            'description' => 'good',
+            'price' => '40',
+            'img' => 'http://capriceeurope.eu/assets/uploads/2020/04/Pescadoo-e1587726895841.jpg',
+        ),
+        array(
+            'id' => 2,
+            'name' => 'riba',
+            'description' => 'good',
+            'price' => '40',
+            'img' => 'http://capriceeurope.eu/assets/uploads/2020/04/Pescadoo-e1587726895841.jpg',
+        ),
+        array(
+            'id' => 2,
+            'name' => 'riba',
+            'description' => 'good',
+            'price' => '40',
+            'img' => 'http://capriceeurope.eu/assets/uploads/2020/04/Pescadoo-e1587726895841.jpg',
+        ),
+        array(
+            'id' => 2,
+            'name' => 'riba',
+            'description' => 'good',
+            'price' => '40',
+            'img' => 'http://capriceeurope.eu/assets/uploads/2020/04/Pescadoo-e1587726895841.jpg',
+        ),
+        array(
+            'id' => 2,
+            'name' => 'riba',
+            'description' => 'good',
+            'price' => '40',
+            'img' => 'http://capriceeurope.eu/assets/uploads/2020/04/Pescadoo-e1587726895841.jpg',
+        ),
+    ),
+    "Vse Riba" => array(
+        array(
+            'id' => 1,
+            'name' => 'myaso',
+            'description' => 'good',
+            'price' => '65',
+            'img' => 'https://cdn.profile.ru/wp-content/uploads/2019/08/shutterstock_1050939104-782x440.jpg',
+        ),
+        array(
+            'id' => 2,
+            'name' => 'riba',
+            'description' => 'good',
+            'price' => '40',
+            'img' => 'http://capriceeurope.eu/assets/uploads/2020/04/Pescadoo-e1587726895841.jpg',
+        ),
+        array(
+            'id' => 3,
+            'name' => 'yaica',
+            'description' => 'good',
+            'price' => '15',
+            'img' => 'https://liter.kz/wp-content/uploads/2020/10/pkyblkr-750x422.jpg',
+        ),
+        array(
+            'id' => 3,
+            'name' => 'yaica',
+            'description' => 'good',
+            'price' => '15',
+            'img' => 'https://liter.kz/wp-content/uploads/2020/10/pkyblkr-750x422.jpg',
+        ), array(
+            'id' => 3,
+            'name' => 'yaica',
+            'description' => 'good',
+            'price' => '15',
+            'img' => 'https://liter.kz/wp-content/uploads/2020/10/pkyblkr-750x422.jpg',
+        ), array(
+            'id' => 3,
+            'name' => 'yaica',
+            'description' => 'good',
+            'price' => '15',
+            'img' => 'https://liter.kz/wp-content/uploads/2020/10/pkyblkr-750x422.jpg',
+        ),
+        array(
+            'id' => 3,
+            'name' => 'yaica',
+            'description' => 'good',
+            'price' => '15',
+            'img' => 'https://liter.kz/wp-content/uploads/2020/10/pkyblkr-750x422.jpg',
+        ),
+        array(
+            'id' => 3,
+            'name' => 'yaica',
+            'description' => 'good',
+            'price' => '15',
+            'img' => 'https://liter.kz/wp-content/uploads/2020/10/pkyblkr-750x422.jpg',
+        ),
+    ),
+    "Vse Yica" => array(
+        array(
+            'id' => 1,
+            'name' => 'myaso',
+            'description' => 'good',
+            'price' => '65',
+            'img' => 'https://cdn.profile.ru/wp-content/uploads/2019/08/shutterstock_1050939104-782x440.jpg',
+        ),
+        array(
+            'id' => 2,
+            'name' => 'riba',
+            'description' => 'good',
+            'price' => '40',
+            'img' => 'http://capriceeurope.eu/assets/uploads/2020/04/Pescadoo-e1587726895841.jpg',
+        ),
+        array(
+            'id' => 3,
+            'name' => 'yaica',
+            'description' => 'good',
+            'price' => '15',
+            'img' => 'https://liter.kz/wp-content/uploads/2020/10/pkyblkr-750x422.jpg',
+        ),
+        array(
+            'id' => 3,
+            'name' => 'yaica',
+            'description' => 'good',
+            'price' => '15',
+            'img' => 'https://liter.kz/wp-content/uploads/2020/10/pkyblkr-750x422.jpg',
+        ), array(
+            'id' => 3,
+            'name' => 'yaica',
+            'description' => 'good',
+            'price' => '15',
+            'img' => 'https://liter.kz/wp-content/uploads/2020/10/pkyblkr-750x422.jpg',
+        ), array(
+            'id' => 3,
+            'name' => 'yaica',
+            'description' => 'good',
+            'price' => '15',
+            'img' => 'https://liter.kz/wp-content/uploads/2020/10/pkyblkr-750x422.jpg',
+        ),
+        array(
+            'id' => 3,
+            'name' => 'yaica',
+            'description' => 'good',
+            'price' => '15',
+            'img' => 'https://liter.kz/wp-content/uploads/2020/10/pkyblkr-750x422.jpg',
+        ),
+        array(
+            'id' => 3,
+            'name' => 'yaica',
+            'description' => 'good',
+            'price' => '15',
+            'img' => 'https://liter.kz/wp-content/uploads/2020/10/pkyblkr-750x422.jpg',
+        ),
+    ),
+);
+?>
+<div class="container mt-5">
+
+    <?php
+    $count = 0;
+    foreach ($categories as $category => $products) {
+        echo '<div class="accordion accordion-flush" id="accordionFlushExample">
+            <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-heading' . $count . '">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse' . $count . '" aria-expanded="false" aria-controls="flush-collapse' . $count . '">
+                ' . $category . '
+                </button>
+            </h2>
+            <div id="flush-collapse' . $count . '" class="accordion-collapse collapse" aria-labelledby="flush-heading' . $count . '" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">
+            <div class="row">';
+        foreach ($products as $product) {
+            echo '<div class="col-sm">';
+            include('elements/card.php');
+            echo '</div>';
+        }
+        echo '</div></div></div></div>';
+        $count++;
+    }
+    ?>
+</div>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+    <?php include_once('navigation.php'); ?>
+    <?php include_once('body.php'); ?>
+
+
+    <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+    <script src="https://www.google-analytics.com/analytics.js" async></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+
+</html>
