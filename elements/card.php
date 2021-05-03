16f3968770ff72df9e05913d8b3d60003e5c544e
Chaim <gchaimke@gmail.com>
2021-05-03 22:02:48 +0300

    remove from cart

--- elements/card.php
+++ elements/card.php
@@ -2,8 +2,8 @@
     <div class="card-image" style="background-image: url('<?= $product->img ?>');"></div>
     <div class="card-body">
         <?php if ($logedin) {
-            echo "<i class='far fa-trash-alt delete-product' data-category='$category_num ' data-product='$product_num '></i>";
-            echo "<i class='far fa-edit edit-product' data-category='$category_num ' 
+            echo "<i class='far fa-trash-alt delete-product text-danger' data-category='$category_num ' data-product='$product_num '></i>";
+            echo "<i class='far fa-edit edit-product text-info' data-category='$category_num ' 
             data-product='$product_num ' 
             data-picture='$product->img' 
             data-name='$product->name'
@@ -13,7 +13,6 @@
             ></i>";
             echo "<i class='far fa-clone duplicate-product' data-category='$category_num ' data-product='$product_num '></i>";
         }
-        echo "<i class='fas fa-cart-plus product-to-cart' data-product='$product->name $product->description' data-price='$product->price'></i>";
         ?>
         <center>
             <h5 class="card-title"><?= $product->name ?></h5>
@@ -21,6 +20,7 @@
             <div class="d-flex justify-content-center">
                 <h5 class="card-text me-3"><?= $product->price ?> ש"ח</h5>
                 <div> <?= $product->kind ?></div>
+                <?= "<i class='fas fa-cart-plus product-to-cart' data-product='$product->name $product->kind' data-price='$product->price'></i>" ?>
             </div>
         </center>
     </div>
--- elements/cart.php
+++ elements/cart.php
@@ -3,7 +3,7 @@
         <center>
             <h6 class="card-title cart_header">Your cart</h6>
         </center>
-        <ul class="cart_items"></ul>
+        <ol class="cart_items"></ol>
         <center>
             <b>Total: <span class="cart-total">0</span> ש"ח</b>
             <div><a class="btn btn-outline-success text-info mt-1 cart-send" aria-hidden="true" href="https://api.whatsapp.com/send/?phone=972<?php echo substr($company->phone,1);?>&text=">Send</a></div>
