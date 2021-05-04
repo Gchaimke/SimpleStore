03c19b8ed4ba952780939ac0bf6aa5367347f32e
Chaim <gchaimke@gmail.com>
2021-05-04 18:02:10 +0300

    gallery ui update

--- elements/gallery.php
+++ elements/gallery.php
@@ -8,11 +8,12 @@ $images = get_images();
     <div class="m-2 gallery_view" style="display: none;">
         <span class='btn btn-close bg-warning close-parent float-end' aria-label="Close"></span>
         <b>Click to select image</b>
-        <div class="d-flex">
+        <div class="row overflow-auto">
             <?php
             foreach ($images as $image) {
-                echo "<div class='card'><img class='gallery_image' src='/img/products/$image' data-path='/img/products/$image' width='100px'>
-                
+                echo "<div class='col gallery_image m-2' data-path='/img/products/$image'>
+                        <span style=\"background-image: url('/img/products/$image');;height: 100px;display: block;width: 100%;min-width: 150px;margin: auto;\"></span>
+                        <div>$image</div>
                 </div>";
             }
             ?>
