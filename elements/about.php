4ef29781fd51e1d601ede2922e3ff9c75e24ec23
Chaim <gchaimke@gmail.com>
2021-05-05 14:34:30 +0300

    add about text

--- elements/about.php
+++ elements/about.php
@@ -7,53 +7,104 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <div>
-                    Много людей используют социальные сети для продаж своих товаров.
-                    Они используют разные способы показать свой список товаров, каждый раз мучаясь сего оформлением и обновлением.
-                    А их клиенты должны переписовать или копировать названия товаров в личную переписку для заказа.
-                    <p>Я сделал простое приложение со следующими функциями:</p>
+                <p>Autor: Chaim Gorbov</p>
+                <p>Version: 1.0</p>
+                <p>Date: 05.05.2021</p>
+                <p>In this project: HTML5, CSS, JS, Jquery, Bootstrap 5, PHP 7.4</p>
+
+                <p>
+                    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#ru" aria-expanded="false" aria-controls="ru">Русский</button>
+                    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#eng" aria-expanded="false" aria-controls="eng ru">English</button>
+                    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#he" aria-expanded="false" aria-controls="he">עברית</button>
+                </p>
+                <div class="col">
+                    <div class="collapse" id="he">
+                        <div class="card card-body rtl">
+                            <div>
+                                הרבה אנשים משתמשים במדיה חברתית כדי למכור את המוצרים שלהם.
+                                הם משתמשים בדרכים שונות כדי להציג את רשימת המוצרים שלהם, כל פעם נאבקים עם העיצוב והעדכון הזה.
+                                ולקוחותיהם צריכים לכתוב מחדש או להעתיק את שמות הסחורות בהתכתבות האישית שלהם לצורך ההזמנה.
+                                <p> הכנתי יישום פשוט עם הפונקציות הבאות: </p>
+                            </div>
+                            <ol>
+                                <h6> למשתמש: </h6>
+                                <li> הוספת עד 10 מוצרים לרשימה </li>
+                                <li> שליחת הרשימה באמצעות WhatsApp באמצעות לחיצה על כפתור השליחה </li>
+                            </ol>
+                            <ol>
+                                <h6> למנהל <b class="text-danger"> לאחר הזנת הסיסמה: </b>
+                                </h6>
+                                <li> עריכת נתוני חנות </li>
+                                <li> הוספה / מחיקה של קטגוריות </li>
+                                <li> ערוך שמות של קטגוריות </li>
+                                <li> הוספה / מחיקה של קטגוריות מוצרים </li>
+                                <li> שיבוט מוצרים </li>
+                                <li> ניהול גלריה </li>
+                                <li> הוספת תמונות מאתר אחר </li>
+                                <li> העלה תמונות מהמחשב או מהטלפון </li>
+                            </ol>
+                        </div>
+                    </div>
                 </div>
-
-                <ol>
-                    <h6>для пользователя:</h6>
-                    <li>добавление до 10 товаров в список</li>
-                    <li>отправка списка по WhatsApp, нажав на кнопку отправки</li>
-                </ol>
-
-                <ol>
-                    <h6>для администратора <b class="text-danger">после ввода пароля:</b></h6>
-                    <li>редактирование данных магазина</li>
-                    <li>добавление/удаление категорий</li>
-                    <li>изправление названия категорий</li>
-                    <li>добавление/удаление товаров категории</li>
-                    <li>клонирование продукта</li>
-                    <li>управление галереей</li>
-                    <li>добавление фотографий с другого сайта</li>
-                    <li>загружать фото с компьютера или телефона</li>
-                </ol>
-
-                <div>
-                    A lot of people use social media to sell their products.
-                    They use different ways to show their list of products, each time struggling with this design and update.
-                    And their customers have to rewrite or copy the names of the goods in their personal correspondence for the order.
-                    <p>I made a simple application with the following functions:</p>
+                <div class="col">
+                    <div class="collapse" id="eng">
+                        <div class="card card-body">
+                            <div>
+                                A lot of people use social media to sell their products.
+                                They use different ways to show their list of products, each time struggling with this design and update.
+                                And their customers have to rewrite or copy the names of the goods in their personal correspondence for the order.
+                                <p>I made a simple application with the following functions:</p>
+                            </div>
+                            <ol>
+                                <h6>for the user:</h6>
+                                <li>adding up to 10 products to the list</li>
+                                <li>sending the list via WhatsApp by clicking on the send button</li>
+                            </ol>
+                            <ol>
+                                <h6>for the administrator <b class="text-danger">after entering the password:</b></h6>
+                                <li>editing store data</li>
+                                <li>adding/deleteing categories</li>
+                                <li>edit names of categories</li>
+                                <li>adding/deleteing products categories</li>
+                                <li>product cloning</li>
+                                <li>gallery management</li>
+                                <li>adding photos from another site</li>
+                                <li>upload photos from computer or phone</li>
+                            </ol>
+                        </div>
+                    </div>
                 </div>
-                <ol>
-                    <h6>for the user:</h6>
-                    <li>adding up to 10 products to the list</li>
-                    <li>sending the list via WhatsApp by clicking on the send button</li>
-                </ol>
-                <ol>
-                    <h6>for the administrator <b class="text-danger">after entering the password:</b></h6>
-                    <li>editing store data</li>
-                    <li>adding/deleteing categories</li>
-                    <li>edit names of categories</li>
-                    <li>adding/deleteing products categories</li>
-                    <li>product cloning</li>
-                    <li>gallery management</li>
-                    <li>adding photos from another site</li>
-                    <li>upload photos from computer or phone</li>
-                </ol>
+                <div class="col">
+                    <div class="collapse show" id="ru">
+                        <div class="card card-body">
+                            <div>
+                                Много людей используют социальные сети для продаж своих товаров.
+                                Они используют разные способы показать свой список товаров,
+                                каждый раз мучаясь с его оформлением и обновлением.
+                                А их клиенты должны переписовать или копировать названия товаров в личную переписку для заказа.
+                                <p>Я сделал простое приложение со следующими функциями:</p>
+                            </div>
+
+                            <ol>
+                                <h6>для пользователя:</h6>
+                                <li>добавление до 10 товаров в список</li>
+                                <li>отправка списка по WhatsApp, нажав на кнопку отправки</li>
+                            </ol>
+
+                            <ol>
+                                <h6>для администратора <b class="text-danger">после ввода пароля:</b></h6>
+                                <li>редактирование данных магазина</li>
+                                <li>добавление/удаление категорий</li>
+                                <li>изправление названия категорий</li>
+                                <li>добавление/удаление товаров категории</li>
+                                <li>клонирование продукта</li>
+                                <li>управление галереей</li>
+                                <li>добавление фотографий с другого сайта</li>
+                                <li>загружать фото с компьютера или телефона</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
