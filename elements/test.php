cfb49353a1a175d1c81b0386d88870343207cdc2
Chaim <gchaimke@gmail.com>
2021-05-24 23:08:48 +0300

    ui update

---
+++ elements/test.php
@@ -0,0 +1,90 @@
+<div class="row p-2">
+    <center>
+        <h3>Дни доставки:</h3>
+    </center>
+    <div class="col-md card m-3 px-2 py-3">
+        <center>
+            <h5>Вторник</h5>
+            <hr>
+            <b>Ришон, Реховот и южнее ( Ашдод, Ашкелон...)</b>
+        </center>
+    </div>
+    <div class="col-md card m-3 px-2 py-3">
+        <center>
+            <h5>Среда</h5>
+            <hr>
+            <b>Бат Ям, Холон</b>
+        </center>
+    </div>
+    <div class="col-md card m-3 px-2 py-3">
+        <center>
+            <h5>Четверг</h5>
+            <hr>
+            <b>Петах Тиква, Кфар Саба, Тель Авив,Рамат Ган</b>
+        </center>
+    </div>
+    <div class="col-md card m-3 px-2 py-3">
+        <center>
+            <h5>Пятница</h5>
+            <hr>
+            <b>Север - Хадера и севернее</b>
+        </center>
+    </div>
+</div>
+<div class="row p-2">
+    <center>
+        <h3>Правила:</h3>
+    </center>
+    <div class="col-md">
+        <ul>
+            <h5><span class="badge bg-danger">Важно !</span> от дня заказа зависит доставка:</h5>
+            <b>
+                <li>Заказы на юг и центр принимаем до воскресенья 21:00</li>
+                <li>Заказы в другие города - до понедельника 21:00</li>
+                <li>Север до вторника 14:00</li>
+            </b>
+        </ul>
+        Свежее мясо из <a target="_blank" href="https://www.google.com/maps/place/Batzra/@32.2169939,34.8634161,13.75z/data=!4m5!3m4!1s0x151d38914f9cbf4d:0x779c66ae82dd4ee2!8m2!3d32.214541!4d34.878123?hl=en">мошава Бацра</a>.
+        Расфасовка заказов в магазине мошава по адресу <a target="_blank" href="https://www.google.com/maps/place/Ze'ev+Orlov+St+55,+Petah+Tikva/@32.094717,34.88324,17z/data=!4m5!3m4!1s0x151d366b391ff9a5:0x6b8b33fddd101bb8!8m2!3d32.094717!4d34.885434?hl=en">Оролов 55 Петах Тиква</a>.
+        Если хотите забрать с магазина, надо сделать заказ за день до приезда!
+    </div>
+    <div class="col-md">
+        <ul>
+            <h5>Минимум заказ для бесплатной доставки:</h5>
+            <b>
+                <li>Центр - Бат Ям, Ришон, Холон, Тель Авив, Петах Тиква, Кфар Саба, Рамат Ган - бесплатная от
+                    <span class="badge bg-success">200₪</span>
+                </li>
+                <li>Юг - Реховот, Лод, Рамле, Ашдод, Ашкелон, Беер Шева - бесплатная от <span class="badge bg-success">300₪</li>
+                <li>Север - Нетания, Хадера, Хайфа, Крайот, Наария, Кармиэль, Афула - бесплатная от <span class="badge bg-success">300₪</span></li>
+            </b>
+        </ul>
+        <ul>
+            Доставка для заказов меньше указоного выше, только если есть еще заказы туда же:
+            <li>то в центре <span class="badge bg-success">30₪</span></li>
+            <li>в удаленные от центра <span class="badge bg-success">50₪</span></li>
+        </ul>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md card m-3 p-2">
+        <center>
+            <h5>Для контактов группы в телеграм: <a target="_blank" href="https://t.me/Quail_Chicken_Chick_Egg_Delivery">App</a> /
+                <a target="_blank" href="https://web.telegram.org/#/im?p=@Quail_Chicken_Chick_Egg_Delivery">WEB</a>
+            </h5>
+        </center>
+    </div>
+
+    <div class="col-md card m-3 p-2">
+        <center>
+            <h5>Вотсап для уточнений: <a target="_blank" href="https://wa.me/972523488059"> 0523488059</a></h5>
+        </center>
+    </div>
+</div>
+<div class="row p-2">
+    <div class="col-md m-3">
+        П.С. Для заказов всех частей свежей баранины и молодого барашка( ягненка) , заказы делать заранее за 3 дня! </br>
+        Баранье мясо более жесткое и рекомендуют не использовать для мангалов. Ягненок - мясо более нежное.
+    </div>
+</div>
