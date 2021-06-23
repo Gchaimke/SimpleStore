adde08e55e7172073ca18a1c0947e8a8ba4d595e
Chaim <gchaimke@gmail.com>
2021-06-23 15:03:31 +0300

    ui fix

--- elements/search_order.php
+++ elements/search_order.php
@@ -1,7 +1,7 @@
 <form id="search_order" class="col-md-4">
     <div class="input-group mt-2">
         <label class="input-group-text" for="search_text"><?=lang("search order")?></label>
-        <input type="text" id="search_order_val" class="form-control" name="search"/>
+        <input type="text" id="search_order_val" class="form-control" name="search_order"/>
         <button type="submit" class="btn btn-dark send"><?=lang("search_btn")?></button>
     </div>
 </form>
