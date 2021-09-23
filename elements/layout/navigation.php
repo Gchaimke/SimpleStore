c023e35d855add0c77374e36b80163fe2f5b34b7
Chaim <gchaimke@gmail.com>
2021-09-23 13:44:06 +0300

    navigation mobile fix

--- elements/layout/navigation.php
+++ elements/layout/navigation.php
@@ -1,20 +1,29 @@
 <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="<?= SITE_ROOT ?>" tabindex="0">
+        <div class="navbar-brand text-wrap text-center">
+            <a href="<?= SITE_ROOT ?>" tabindex="0"></a>
             <img src="<?= $company->logo ?>" alt="" width="30" height="24" class="d-inline-block align-text-top">
-            <?= $company->name ?> <?= $company->phone ?></a>
+            <a href="<?= SITE_ROOT ?>" tabindex="0"> <?= $company->name ?></a>
+            <a href="tel:<?= $company->phone ?>" tabindex="0"> <?= $company->phone ?></a>
+        </div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler02" aria-controls="navbarToggler02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarToggler02">
             <div class="navbar-nav me-auto mb-2 mb-lg-0">
                 <?php if ($logedin) : ?>
-                    <span id="admin_nav">
-                        <input type="button" class="btn btn-outline-warning align-middle mx-md-2 my-2 my-md-0 col" value="<?= lang("settings") ?>" data-bs-toggle="modal" data-bs-target="#editCompany" tabindex="0">
-                        <input type="button" class="btn btn-outline-success align-middle mx-md-2 category_editor_toggle col text-white" value="<?= lang("new_category") ?>" data-bs-toggle="modal" data-bs-target="#categoryEditor" tabindex="0">
-                        <a class="btn btn-outline-info align-middle mx-md-2 col" href="<?='?orders='.date('my')?>" tabindex="0"><?= lang("orders") ?></a>
+                    <span id="admin_nav" class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle btn btn-outline-success text-white" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <?= lang("settings") ?>
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <?php include_once('elements/statistic.php'); ?>
+                            <span class="dropdown-item" data-bs-toggle="modal" data-bs-target="#editCompany" tabindex="0"><?= lang("settings") ?></span>
+                            <span class="dropdown-item" data-bs-toggle="modal" data-bs-target="#categoryEditor" tabindex="0"><?= lang("new_category") ?></span>
+                            <a class="dropdown-item" href="<?= '?orders=' . date('my') ?>" tabindex="0"><?= lang("orders") ?></a>
+
+                        </ul>
                     </span>
-                    <?php include_once('elements/statistic.php'); ?>
                 <?php endif ?>
             </div>
             <?php if ($lng == 'ru') : ?>
