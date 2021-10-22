1c4030cdd210200a9c15ca9112208e0c9adc564a
Chaim <gchaimke@gmail.com>
2021-10-22 16:48:03 +0300

    version update

--- functions/helper.php
+++ functions/helper.php
@@ -9,7 +9,7 @@ session_start();
 /**
  * Config
  */
-define("VERSION", "2.1");
+define("VERSION", "2.2");
 
 require_once(__DIR__ . '/../config.php');
 define("ORDERS_PATH", DATA_ROOT . "orders/");
